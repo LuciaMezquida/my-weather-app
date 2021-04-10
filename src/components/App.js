896fc654e208335d7a3bea6f97e61fb2b5ce6ac6
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-04-10 18:45:27 +0200

    Add fetch call into api.js and export it to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import '../styles/App.css';
 import React, { useEffect, useState } from "react";
-
+import apiCall from '../services/api'
 
 function App() {
   const [latitude, setLatitude] = useState([]);
@@ -8,22 +8,17 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // const fetchData = async () => {
+    const fetchData = async () => {
       navigator.geolocation.getCurrentPosition((position) => {
         setLatitude(position.coords.latitude);
         setLongitude(position.coords.longitude);
       });
-      console.log("Latitude is:", latitude)
-      console.log("Longitude is:", longitude)
-      console.log(navigator.geolocation);
-    //   await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longigude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    //   .then(res => res.json())
-    //   .then(result => {
-    //     setData(result)
-    //     console.log(result);
-      // });
-    // }
-    // fetchData();
+      await apiCall(latitude, longitude).then(result => {
+        setData(result)
+        console.log(result)
+      });
+    }
+    fetchData();
   }, [latitude,longitude])
   return (
     <h1 className="App">
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+'use strict'
+
+export default function apiCall (latitude, longitude) {
+  return fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+  .then(response => response.json())
+} 
+
